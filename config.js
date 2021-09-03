85f3a0e1d164546777cfa04d3aeb755b17282b27
Your Name <you@example.com>
2021-09-03 09:57:17 +0200

    change db location

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.QOVERY_MONGODB_Z471928CE_DATABASE_URL,
   watsonVersion: process.env.TRANSLATION_VERSION,
   watsonAPIKey: process.env.TRANSLATION_API_KEY,
   watsonURL: process.env.TRANSLATION_URL,
