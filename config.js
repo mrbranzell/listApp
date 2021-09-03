efdf48dadb7c16a8c529eb6ffc33534aba518c9b
Your Name <you@example.com>
2021-09-03 10:48:24 +0200

    -

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
+const password = process.env.MONGO_PASSWORD
+const database = process.env.MONGO_DATABASE
+const db = `mongodb+srv://learnNode:${password}@cluster0.qmark.mongodb.net/${database}?retryWrites=true&w=majority`
+
 module.exports = {
   port: process.env.PORT || 3000,
-  mongoURI: process.env.QOVERY_MONGODB_Z471928CE_DATABASE_URL,
+  mongoURI: db,
   watsonVersion: process.env.TRANSLATION_VERSION,
   watsonAPIKey: process.env.TRANSLATION_API_KEY,
   watsonURL: process.env.TRANSLATION_URL,
